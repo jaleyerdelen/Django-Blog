a4cea4fc4bd42348f3b3efb466a825fe55dd33b2
jaleyerdelen <jaleyerdelen@gmail.com>
2022-12-24 16:21:31 +0300

    blog detail page

--- blog/views.py
+++ blog/views.py
@@ -42,9 +42,20 @@ def blogs(request):
     }
     return render(request,"blog/blogs.html", context)
 
-def blog_details(request,id):
+def blog_details(request, id):
+    blogs = data["blogs"]
+    selectedBlog = None
+ 
+    for blog in blogs:
+        if blog["id"] == id:
+            selectedBlog = blog
+
+    #Comprehension
+    # blogs = data["blogs"]
+    # selectedBlog = [blog for blog in blogs if blog["id"] == id[0]]
+
     return render(request,"blog/blog-details.html", {
-        "id":id
+        "blog":selectedBlog
     })
 
 
