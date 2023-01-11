f32de35877505191387e7648ab84a1bf8cb006af
jaleyerdelen <jaleyerdelen@gmail.com>
2023-01-11 16:08:38 +0300

    create logout and update page

--- account/views.py
+++ account/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+
 def login_request(request):
+    if login.user.is_authenticated:
+        return("home")
+
     if request.method == 'POST':
         username = request.POST["username"]
         password = request.POST["password"]
@@ -19,6 +23,9 @@ def login_request(request):
     return render(request, "account/login.html")
 
 def register_request(request):
+    if login.user.is_authenticated:
+        return("home")
+
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
@@ -67,4 +74,5 @@ def register_request(request):
     return render(request, "account/register.html")
 
 def logout_request(request):
+    logout(request)
     return redirect("home")
