a55e4f5e30c4f29714255da9d7274c7c67ddc91a
jaleyerdelen <jaleyerdelen@gmail.com>
2023-01-11 14:50:46 +0300

    create register and login

--- account/views.py
+++ account/views.py
@@ -1,9 +1,69 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 def login_request(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "account/login.html", {
+                "error": "Invalid username or password"
+            })
+
     return render(request, "account/login.html")
 
 def register_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+
+            if User.objects.filter(username=username).exists():
+                return render(request, "account/register.html", 
+                {
+                    "error": "username is exists",
+                    "username": username,
+                    "email": email,
+                    "firstname": firstname,
+                    "lastname": lastname
+                })
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, "account/register.html",
+                    {
+                        "error": "password is exist",
+                        "username": username,
+                        "email": email,
+                        "firstname": firstname,
+                        "lastname": lastname
+                    })
+                else:
+                    user = User.objects.create_user(username=username, email=email, first_name=firstname,
+                    last_name = lastname, password=password)
+                    user.save()
+                    return redirect("login")
+        else:       
+            return render(request, "account/register.html", 
+            {
+                "error": "Passwords do not match", 
+                "username": username,
+                "email": email,
+                "firstname": firstname,
+                "lastname": lastname
+            })
+
+
     return render(request, "account/register.html")
 
 def logout_request(request):
